9807d34bfa9fc07fbc1e4da1a77e78052ca880c2
cworgan <cworgan@gmail.com>
2024-06-15 21:57:52 -0400

    change to test closures

--- testfile.js
+++ testfile.js
@@ -1,15 +1,14 @@
-//test
 // this is a test file
-const jsDateAnnouncement = new Date(818031600000);
-const currentDate = new Date();
-const diff = jsDateAnnouncement - currentDate;
-const formatter = new Intl.RelativeTimeFormat('en', {
-    numeric: 'auto'
-});
-const diffInDays = Math.round(diff / 86400000);
-const diffInYears = Math.round(diffInDays / 365);
-const diffInText = formatter.format(diffInDays, 'day');
-console.log(`JavaScript was presented to the world ${formatter.format(diffInDays, 'day')}`);
-// JavaScript was presented to the world 10,094 days ago
-console.log(`JavaScript was presented to the world ${formatter.format(diffInYears, 'year')}`);
-// JavaScript was presented to the world 28 years ago.
+///////////////////////
+
+const createCounter = (initialValue = 0) => {
+    let counter = initialValue
+    return (incrementalValue) => {
+    counter += incrementalValue
+    console.log(counter)
+    }
+}
+
+const addToCounter = createCounter(10)
+addToCounter(12)
+addToCounter(1)
