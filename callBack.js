08c3e16cf919c29c75a89cd08404ce6076bc536a
cworgan <cworgan@gmail.com>
2024-06-16 20:40:31 -0400

    added new examples to the callBack script

--- callBack.js
+++ callBack.js
@@ -1,3 +1,4 @@
+// Simple callback
 const doSomething = (cb) => {
       console.log('Doing something...');
       cb();
@@ -7,3 +8,52 @@ const nextStep = () => {
     console.log('Callback called');
     };
     doSomething(nextStep);
+
+// Using setTimeout
+console.log('Before setTimeout');
+const secondInMilliseconds = 1000;
+setTimeout(() => {
+  console.log('A second has passed');
+}, secondInMilliseconds);
+console.log('after setTimeout');
+
+// Using setInterval
+//const secondInMilliseconds = 1000;
+// let totalExecutions = 0
+// console.log('Before setInterval');
+// setInterval(() => {
+//     totalExecutions++;
+//     console.log(`A second has passed, this is the ${totalExecutions} execution`);
+// }, secondInMilliseconds);
+// console.log('After setInterval');
+
+// Async promise example
+const randomTimeOutPromise = () => {
+    return new Promise((resolve, reject) => {
+    const time = Math.floor(Math.random() * 100);
+    setTimeout(() => {
+      console.log(`Promise resolved after ${time}ms`);
+      resolve(time);
+      }, time);
+    });
+};
+
+Promise.all([
+    randomTimeOutPromise(),
+    randomTimeOutPromise(),
+    randomTimeOutPromise(),
+    randomTimeOutPromise(),
+    randomTimeOutPromise(),
+    ]).then((results) => {
+      console.log("results:", results);
+    });
+
+Promise.race([
+    randomTimeOutPromise(),
+    randomTimeOutPromise(),
+    randomTimeOutPromise(),
+    randomTimeOutPromise(),
+    randomTimeOutPromise(),
+    ]).then((result) => {
+    console.log("result:", result);
+    });
